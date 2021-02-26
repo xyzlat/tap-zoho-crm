f0d858aa6e5fd68d0d29cfa3ea1471170566691c
Johannes Valbjorn <johannes.valbjorn@gmail.com>
2021-02-26 10:49:06 +0100

    downgrade to common dateutil version

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="tap-zoho-crm",
-    version="0.0.2",
+    version="0.0.3",
     description="tap to fetch zoho crm API",
     py_modules=["tap_zoho_crm"],
     install_requires=[
         "backoff==1.8.0",
         "requests==2.22.0",
         "singer-python==5.8.1",
-        "python-dateutil==2.8.1"
+        "python-dateutil==2.8.0"
     ],
     entry_points="""
           [console_scripts]
