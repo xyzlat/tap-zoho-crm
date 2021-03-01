0887cdf77eca4121582840c8498263f0337f25eb
Johannes Valbjorn <johannes.valbjorn@gmail.com>
2021-03-01 09:58:34 +0100

    require api_domain not access_token

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="tap-zoho-crm",
-    version="0.0.3",
+    version="0.0.4",
     description="tap to fetch zoho crm API",
     py_modules=["tap_zoho_crm"],
     install_requires=[
